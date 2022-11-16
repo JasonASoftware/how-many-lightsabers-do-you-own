d7065e7e3316a8aa376828cb52edb9bc4bd2fce8
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-11-15 21:00:03 -0500

    created js file with the code

---
+++ lightSabers.js
@@ -0,0 +1,11 @@
+//write a function with conditonals to test how many light sabers each person has
+
+// created a function with a parameter called name
+function howManyLightsabersDoYouOwn(name) {
+  //if conditionals with the parameter if the name is zach it returns 18 sabers and the rest will return 0
+  if( name === "Zach"){
+    return 18
+  }else {
+    return 0
+  } 
+}
